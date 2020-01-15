6493ac39261bc49605cd343103049d9418423cbf
sunshine <mytrade1086@gmail.com>
2020-01-15 23:18:28 +0530

    userdefined method calll added

---
+++ src/main/java/com/resurrectionSeleniumARF/SumitUtility.java
@@ -0,0 +1,40 @@
+package com.resurrectionSeleniumARF;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SumitUtility {
+
+//static WebDriver driver;
+	
+	
+
+//public SumitUtility(WebDriver driver) {
+//		this.driver=driver;
+//	}
+
+	/*
+	 * public static boolean isElementPresent(By by) {
+	 * 
+	 * try { driver.findElement(by); System.out.println("Webelement present");
+	 * return true; } catch (Exception e) {
+	 * System.out.println("No Element present"); return false; }
+	 * 
+	 * }
+	 */
+
+
+public static boolean isElementPresent(WebDriver driver,By by) {
+try {
+		 driver.findElement(by);
+		System.out.println("Webelement present");
+		return true;
+	} catch (Exception e) {
+		System.out.println("No Element present");
+		return false;
+	}
+
+}
+
+}
---
+++ src/main/java/com/resurrectionSeleniumARF/isPresent_isDisplayed_isEnabled.java
@@ -0,0 +1,37 @@
+package com.resurrectionSeleniumARF;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class isPresent_isDisplayed_isEnabled extends SumitUtility {
+	
+	static WebDriver driver;
+	public isPresent_isDisplayed_isEnabled(WebDriver driver) {
+		//super(driver);
+	}
+	public static void main(String[] args) {	
+		
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		isPresent_isDisplayed_isEnabled ipe=new isPresent_isDisplayed_isEnabled(driver);
+		
+		driver.get("https://www.wikipedia.org/");
+		
+		isElementPresent(driver,By.xpath("//div[@class='central-textlogo-wrapper']"));	
+		
+		
+		WebElement langDropDown=driver.findElement(By.id("searchLanguage"));
+		System.out.println(langDropDown.isDisplayed()); // false as list is kinda of hidden
+		System.out.println(langDropDown.isEnabled());		
+		System.out.println(langDropDown.isSelected());
+
+	}
+	
+	
+	}
+
+
